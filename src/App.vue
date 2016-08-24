e6365c443b7aca942406de9453bf20b17cf192bf
Jackson Liu <416883813@qq.com>
2016-08-24 14:55:55 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+  <div id='building'></div>
+  <div id='building-back'></div>
+  </div>
+</template>
+
+<script>
+import ground from './components/ground.vue'
+
+export default {
+  data () {
+    return {
+      msg: 'vue flappybird!'
+    }
+  },
+  components () {
+    return {
+      ground
+    }
+  }
+}
+</script>
+
+<style>
+@import './assets/css/normalize.css';
+
+* {
+  box-sizing: border-box;
+}
+html {
+  height: 100%;
+}
+
+body {
+  font-family: Helvetica, sans-serif;
+  height: 100%;
+}
+
+#app {
+  width: 650px;
+  height: 900px;
+  margin: 0 auto;
+  border: 5px solid black;
+  background: url(./assets/img/bg_day.png) no-repeat;
+  background-size: 100% 100%;
+}
+
+@media screen and (max-width: 650px) {
+  #app {
+    width: 100%;
+    height: 100%;
+    border: none;
+  }
+}
+
+</style>
---
+++ src/components/ground.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id='ground'></div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+#ground {
+}
+</style>
