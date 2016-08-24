4be8efe03f519a4f594ebfe83dd1cf49b4c9f5d4
Jackson Liu <416883813@qq.com>
2016-08-24 17:38:21 +0800

    add animation stop

--- src/animation.js
+++ src/animation.js
@@ -5,7 +5,13 @@ const animation = new EventEmitter()
 export default animation
 
 animation.intertime = 50
+animation.move = () => animation.emit('move')
 
 animation.on('start', () => {
-  setInterval(() => animation.emit('move'), animation.intertime)
+  animation.timer = setInterval(animation.move, animation.intertime)
+  setInterval(() => animation.emit('stop'), 5000)
+})
+
+animation.on('stop', () => {
+  clearInterval(animation.timer)
 })
